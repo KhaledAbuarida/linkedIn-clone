bd6f338f901a6601430a1bda86257c26bef2e5da
Khaled Mahmoud Abu-3rida <khaledabuarida0@gmail.com>
2024-04-28 22:03:23 +0300

    app header

---
+++ src/app/app-header/app-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-header.component.html',
+  styleUrl: './app-header.component.css'
+})
+export class AppHeaderComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AppHeaderComponent } from './app-header/app-header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppHeaderComponent },
+];
+
