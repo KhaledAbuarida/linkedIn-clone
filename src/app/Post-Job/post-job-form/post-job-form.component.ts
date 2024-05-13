896cf72a01ef1d62051a97f4e4d73c0fc5a3f6b1
Hassan Ahmed <ahmedhassanmana@gmail.com>
2024-05-13 19:29:19 +0300

    Add Job functionallity and findCombanybyname function

--- src/app/Post-Job/post-job-form/post-job-form.component.ts
+++ src/app/Post-Job/post-job-form/post-job-form.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { JobService } from '../../services/job.service';
+import { Job } from '../../model/Job';
+import { CompanyService } from '../../services/company.service';
+import { Company } from '../../model/company';
 
 @Component({
   selector: 'app-post-job-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './post-job-form.component.html',
   styleUrl: './post-job-form.component.css'
 })
 export class PostJobFormComponent {
+  title: string = '';
+  company: string = '';
+  location: string = '';
+  description: string = '';
+  benefits: string = '';
+  responsibilities: string = '';
+  qualifications: string = '';
+
+  constructor(private router: Router, public jobservice: JobService, public companyservice: CompanyService) { }
+
+  async addJob() {
+    const job = new Job();
+
+    try {
+      const Company = await this.companyservice.findCompanybyname(this.company);
+      job.company = Company;
+    } catch (error) {
+      console.error("Error fetching company:", error);
+    }
+
+    job.title = this.title
+
+    job.location = this.location
+    job.description = this.description
+    job.benefits = []
+    job.responsibilities = []
+    job.qualifications = []
+
+    try {
+      this.jobservice.addJob(job);
+    } catch (error) {
+      console.log(error)
+    }
+
+    this.router.navigate(['manage-jobs'])
+  }
+
 
 }
--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Firestore, addDoc, collection, collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs/internal/Observable';
-import { Job } from '../model/job';
+import { Job } from '../model/Job';
 
 @Injectable({
   providedIn: 'root'
@@ -9,21 +9,21 @@ import { Job } from '../model/job';
 export class JobService {
 
   constructor(
-    private firestore : Firestore ) { }
-  
-    getJobs() : Observable<Job[]> {
-      const jobCollection = collection(this.firestore,'jobs');
-      const job = collectionData(jobCollection,{idField:'id'});
-      return job as Observable<Job[]>;
-    }
+    private firestore: Firestore) { }
 
-    addJob(job:Job) {
-      const jobCollection = collection(this.firestore,'jobs');
-      addDoc(jobCollection,{
-        ...job
-       });
-    }
-    
+  getJobs(): Observable<Job[]> {
+    const jobCollection = collection(this.firestore, 'jobs');
+    const job = collectionData(jobCollection, { idField: 'id' });
+    return job as Observable<Job[]>;
+  }
+
+  addJob(job: Job) {
+    const jobCollection = collection(this.firestore, 'jobs');
+    addDoc(jobCollection, {
+      ...job
+    });
+  }
+
 
 
 
